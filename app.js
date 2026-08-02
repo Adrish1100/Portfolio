d53d7ada2e78e325456d0a8649d787e41851e4d7
Adrish Datta <adrishdatta2012@gmail.com>
2026-08-02 14:31:17 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,2 @@
+/* Setup and start animation! */
+var typed = new Typed('#element', { strings: ['<i>Web</i> Developer.','C Programmer.'], typeSpeed: 100, loop:true, backSpeed:100, backDelay:2000}); 
